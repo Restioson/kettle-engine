796ebe09ef55cf87d9dd108e21f5d8072363d869
Restioson <restiosondev@gmail.com>
2017-05-20 08:29:54 +0200

    Updated Java to 1.8 minimum, now uses `self`, and added KTX-Async and assets

--- core/src/io/github/restioson/kettle/Kettle.kt
+++ core/src/io/github/restioson/kettle/Kettle.kt
@@ -2,6 +2,7 @@ package io.github.restioson.kettle
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
@@ -10,22 +11,26 @@ class Kettle : ApplicationAdapter() {
 
     internal lateinit var batch: SpriteBatch
     internal lateinit var img: Texture
+    internal lateinit var assetManager: AssetManager
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        this.batch = SpriteBatch()
+        this.img = Texture("badlogic.jpg")
     }
 
     override fun render() {
+
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+
+        this.batch.begin()
+        this.batch.draw(this.img, 0f, 0f)
+        this.batch.end()
+
     }
 
     override fun dispose() {
-        batch.dispose()
-        img.dispose()
+        this.batch.dispose()
+        this.img.dispose()
     }
 }
